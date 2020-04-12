22c9a73b5105d2eb60710e9a9654df437a63d411
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-04-13 00:12:43 +1000

    - Implemented code to display list.

--- app/src/main/java/anand/example/mvvmsample/fragments/FactsListFragment.kt
+++ app/src/main/java/anand/example/mvvmsample/fragments/FactsListFragment.kt
@@ -1,43 +1,78 @@
 package anand.example.mvvmsample.fragments
 
+import anand.example.mvvmsample.R
+import anand.example.mvvmsample.adapters.FactListAdapter
+import anand.example.mvvmsample.viewmodels.FactsViewModel
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-
-import anand.example.mvvmsample.R
-import anand.example.mvvmsample.model.FactsResponse
-import anand.example.mvvmsample.viewmodels.FactsViewModel
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_facts_list.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class FactsListFragment : Fragment() {
     private lateinit var factsViewModel: FactsViewModel
+    private lateinit var factsAdapter: FactListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         factsViewModel = ViewModelProvider(this).get(FactsViewModel::class.java)
-        factsViewModel.getTitleWithFacts().observe(this, Observer<FactsResponse> { factsResponse ->
-            Toast.makeText(requireContext(), "OnChanged!!!", Toast.LENGTH_LONG).show()
-        })
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facts_list, container, false)
+    ) = inflater.inflate(R.layout.fragment_facts_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initObserver() {
+        factsViewModel.getTitleWithFacts().observe(viewLifecycleOwner, Observer { factsResponse ->
+            swipeRefresh.isRefreshing = false
+            factsResponse?.let { factsAdapter.updateList(factsResponse.rows) }
+        })
+
+        factsViewModel.getDataLoading().observe(viewLifecycleOwner, Observer {
+            if (it && progressBar.visibility == VISIBLE) {
+                progressBar.visibility = GONE
+                recyclerView.visibility = VISIBLE
+            } else {
+                swipeRefresh.isRefreshing = it
+            }
+        })
     }
 
     override fun onResume() {
         super.onResume()
+        recyclerView.visibility = GONE
+        progressBar.visibility = VISIBLE
         factsViewModel.fetchFacts()
     }
 
+    private fun initUi() {
+        factsAdapter = FactListAdapter(mutableListOf())
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = factsAdapter
+        swipeRefresh.setOnRefreshListener {
+            swipeRefresh.isRefreshing = true
+            factsViewModel.fetchFacts()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initObserver()
+    }
 }
