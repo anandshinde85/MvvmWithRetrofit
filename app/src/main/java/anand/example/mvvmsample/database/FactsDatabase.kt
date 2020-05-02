fb2563ae663db6e6f3fedf3c6efa317949893452
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-05-02 22:46:36 +1000

    - Implemented singleton database for app.

--- app/src/main/java/anand/example/mvvmsample/database/FactsDatabase.kt
+++ app/src/main/java/anand/example/mvvmsample/database/FactsDatabase.kt
@@ -1,35 +1,30 @@
 package anand.example.mvvmsample.database
 
-//import anand.example.mvvmsample.database.entities.RowRoom
-//import android.app.Application
-//import androidx.room.Database
-//import androidx.room.Room
-//import androidx.room.RoomDatabase
-//
-//@Database(entities = [RowRoom::class], version = 1)
-//abstract class FactsDatabase : RoomDatabase() {
-//    abstract fun rowDao(): RowDao
-//
-//    // 2
-//    companion object {
-//        private val lock = Any()
-//        private const val DB_NAME = "Fact.db"
-//        private var INSTANCE: FactsDatabase? = null
-//
-//        // 3
-//        fun getInstance(application: Application): FactsDatabase {
-//            synchronized(lock) {
-//                if (INSTANCE == null) {
-//                    INSTANCE =
-//                        Room.databaseBuilder(
-//                            application,
-//                            FactsDatabase::class.java, DB_NAME
-//                        )
-//                            .allowMainThreadQueries()
-//                            .build()
-//                }
-//                return INSTANCE!!
-//            }
-//        }
-//    }
-//}
+import anand.example.mvvmsample.database.daos.FactsDao
+import anand.example.mvvmsample.model.FactsResponse
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [FactsResponse::class], version = 1)
+abstract class FactsDatabase : RoomDatabase() {
+    abstract fun factsDao(): FactsDao
+
+    companion object {
+        private val lock = Any()
+        private const val DB_NAME = "Fact.db"
+        @Volatile private var instance: FactsDatabase? = null
+
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            FactsDatabase::class.java, DB_NAME
+        ).build()
+    }
+}
