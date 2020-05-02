03ba83d5d20776faa536b73139af235fa0438dc9
Anand Shinde <anand.shinde.2@team.telstra.com>
2020-05-02 22:52:58 +1000

    - Made response to be used as entity for storing into database.

--- app/src/main/java/anand/example/mvvmsample/model/FactsResponse.kt
+++ app/src/main/java/anand/example/mvvmsample/model/FactsResponse.kt
@@ -1,13 +1,26 @@
 package anand.example.mvvmsample.model
 
+import anand.example.mvvmsample.helpers.RowsStateConverter
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "facts_table")
+@TypeConverters(RowsStateConverter::class)
 @Parcelize
 data class FactsResponse(
+    @ColumnInfo
     val title: String,
     val rows: List<Rows>
-) : Parcelable
+) : Parcelable {
+    @IgnoredOnParcel
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
 
 @Parcelize
 data class Rows(
